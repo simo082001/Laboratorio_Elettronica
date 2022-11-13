aa569f36864682e0936c7ea6849f598b197c6bdb
Simone Garnero <simonegarnero2@gmail.com>
2022-11-13 12:43:43 +0100

    fatto un po; di robine, funziona ma bisogna continuare

--- Prova_1/Caratteristica.cpp
+++ Prova_1/Caratteristica.cpp
@@ -1,4 +1,5 @@
 #include <cmath>
+#include <vector>
 
 void setStyle() {
   gROOT->SetStyle("Plain");
@@ -20,14 +21,16 @@ void caratteristica() {
   TGraphErrors* silicio = new TGraphErrors("Dati_Silicio.txt", "%lg %lg %*lg %*lg");
   TGraphErrors* germanio = new TGraphErrors("Dati_Gemanio.txt", "%lg %lg %*lg %*lg");
   TGraphErrors* calibrazione = new TGraphErrors("Calibrazione.txt", "%lg %lg %*lg %*lg");
+  TGraphErrors* silicioLog = new TGraphErrors("Dati_Silicio.txt", "%lg %lg %*lg %*lg");
+  TGraphErrors* germanioLog = new TGraphErrors("Dati_Gemanio.txt", "%lg %lg %*lg %*lg");
 
   // Assegno le funznioni di fit
   TF1* f1 = new TF1("silicio", FitFunction, 350, 760, 2);
   TF1* f2 = new TF1("germanio", FitFunction, 72, 360, 2);
 
   // Devo settare i parametri iniziali
-  f1->SetParameters(5,100);
-  f2->SetParameters(4,6);
+  f1->SetParameters(5, 100);
+  f2->SetParameters(4, 6);
 
   // Cosmetica
   silicio->SetTitle("Curva caratteristica silicio");
@@ -39,7 +42,6 @@ void caratteristica() {
   germanio->SetMarkerStyle(7);
   germanio->SetMarkerColor(kRed);
   germanio->SetMarkerSize(4);
-
 
   f1->SetLineColor(kRed);
   f1->SetLineStyle(1);
@@ -66,20 +68,30 @@ void caratteristica() {
   germanio->Fit(f2, "R, C");
 
   // Volendo c'è da creare le varie Canva
-  TCanvas* myCanvas1 = new TCanvas();
-  TCanvas* myCanvas2 = new TCanvas();
-  TCanvas* myCanvas3 = new TCanvas();
+  TCanvas* myCanvas1 = new TCanvas("Caratteristica silicio");
+  TCanvas* myCanvas2 = new TCanvas("Caratteristica silicio scala logaritmica");
+  TCanvas* myCanvas3 = new TCanvas("Caratteristica germanio");
+  TCanvas* myCanvas4 = new TCanvas("Caratteristica germanio scala logaritmica");
+  TCanvas* myCanvas5 = new TCanvas("Retta di calibrazione");
 
-  myCanvas1->SetGrid();
-  myCanvas2->SetGrid();
-  myCanvas3->SetGrid();
+  std::vector<TCanvas*> Canvas = {myCanvas1, myCanvas2, myCanvas3, myCanvas4, myCanvas5};
+  for (int i = 0; i < 0; i++) {
+    Canvas[i]->SetGrid();
+  }
 
   // Disegno i grafici
   gStyle->SetOptFit(111);
   myCanvas1->cd();
   silicio->Draw("APE, SAME");
   myCanvas2->cd();
+  gPad->SetLogy();
+  silicioLog->Draw("APE, SAME");
+  myCanvas3->cd();
   germanio->Draw("APE, SAME");
+  myCanvas4->cd();
+  germanioLog->Draw("APE, SAME");
+  myCanvas5->cd();
+  calibrazione->Draw("APE, SAME");
   f1->Draw("SAME, C");
   f2->Draw("SAME, C");
 }
