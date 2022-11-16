622b58d9ee171d0feb8e65945222083432a3a810
Simone Piraccini <simopira01@gmail.com>
2022-11-16 08:08:31 +0100

    completato lo scheletro con il grafico del germanio

--- Prova_1/Caratteristica.cpp
+++ Prova_1/Caratteristica.cpp
@@ -16,21 +16,31 @@ Double_t FitFunction(Double_t* x, Double_t* par) {
   return val;
 }
 
-void caratteristica() {
+// Scriviamo anche la retta. Sono molto simpatico XD
+Double_t retta(Double_t* x, Double_t* par) {
+  Double_t ics = x[0];
+  Double_t emme = par[0];
+  Double_t qu = par[1];
+  Double_t val = emme * ics + qu;
+  return val;
+}
+
+void Caratteristica() {
   // Definisco i grafici
   TGraphErrors* silicio = new TGraphErrors("Dati_Silicio.txt", "%lg %lg %*lg %*lg");
-  TGraphErrors* germanio = new TGraphErrors("Dati_Gemanio.txt", "%lg %lg %*lg %*lg");
+  TGraphErrors* germanio = new TGraphErrors("Dati_Germanio.txt", "%lg %lg %*lg %*lg");
   TGraphErrors* calibrazione = new TGraphErrors("Calibrazione.txt", "%lg %lg %*lg %*lg");
   TGraphErrors* silicioLog = new TGraphErrors("Dati_Silicio.txt", "%lg %lg %*lg %*lg");
-  TGraphErrors* germanioLog = new TGraphErrors("Dati_Gemanio.txt", "%lg %lg %*lg %*lg");
+  TGraphErrors* germanioLog = new TGraphErrors("Dati_Germanio.txt", "%lg %lg %*lg %*lg");
 
   // Assegno le funznioni di fit
   TF1* f1 = new TF1("silicio", FitFunction, 350, 760, 2);
   TF1* f2 = new TF1("germanio", FitFunction, 72, 360, 2);
+  TF1* f3 = new TF1("retta", retta, 1, 0, 2);
 
   // Devo settare i parametri iniziali
   f1->SetParameters(5, 100);
-  f2->SetParameters(4, 6);
+  f2->SetParameters(5, 100);
 
   // Cosmetica
   silicio->SetTitle("Curva caratteristica silicio");
@@ -43,6 +53,11 @@ void caratteristica() {
   germanio->SetMarkerColor(kRed);
   germanio->SetMarkerSize(4);
 
+  calibrazione->SetTitle("Retta di calibrazione");
+  calibrazione->SetMarkerStyle(7);
+  calibrazione->SetMarkerColor(kBlue);
+  calibrazione->SetMarkerSize(4);
+
   f1->SetLineColor(kRed);
   f1->SetLineStyle(1);
   f1->SetLineWidth(1);
@@ -52,6 +67,11 @@ void caratteristica() {
   f2->SetLineStyle(1);
   f2->SetLineWidth(1);
   f2->SetNpx(1000000);
+
+  f3->SetLineColor(kRed);
+  f3->SetLineStyle(1);
+  f3->SetLineWidth(1);
+  f3->SetNpx(1000000);
 
   silicio->GetYaxis()->SetTitleOffset(1);
   silicio->GetXaxis()->SetTitleOffset(1.2);
@@ -66,13 +86,14 @@ void caratteristica() {
   // Fit dei grafici
   silicio->Fit(f1, "R, C");
   germanio->Fit(f2, "R, C");
+  calibrazione->Fit(f3, "R, C");
 
   // Volendo c'è da creare le varie Canva
-  TCanvas* myCanvas1 = new TCanvas("Caratteristica silicio");
-  TCanvas* myCanvas2 = new TCanvas("Caratteristica silicio scala logaritmica");
-  TCanvas* myCanvas3 = new TCanvas("Caratteristica germanio");
-  TCanvas* myCanvas4 = new TCanvas("Caratteristica germanio scala logaritmica");
-  TCanvas* myCanvas5 = new TCanvas("Retta di calibrazione");
+  TCanvas* myCanvas1 = new TCanvas("Caratteristica silicio", "canvas 1");
+  TCanvas* myCanvas2 = new TCanvas("Caratteristica silicio scala logaritmica", "canvas 2");
+  TCanvas* myCanvas3 = new TCanvas("Caratteristica germanio", "canvas 3");
+  TCanvas* myCanvas4 = new TCanvas("Caratteristica germanio scala logaritmica", "canvas 4");
+  TCanvas* myCanvas5 = new TCanvas("Retta di calibrazione", "canvas 5");
 
   std::vector<TCanvas*> Canvas = {myCanvas1, myCanvas2, myCanvas3, myCanvas4, myCanvas5};
   for (int i = 0; i < 0; i++) {
@@ -89,9 +110,8 @@ void caratteristica() {
   myCanvas3->cd();
   germanio->Draw("APE, SAME");
   myCanvas4->cd();
+  gPad->SetLogy();
   germanioLog->Draw("APE, SAME");
   myCanvas5->cd();
   calibrazione->Draw("APE, SAME");
-  f1->Draw("SAME, C");
-  f2->Draw("SAME, C");
 }
