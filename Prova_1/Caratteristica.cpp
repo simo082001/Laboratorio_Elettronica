2fd87f09732a277d5192a0557fa52601f86719c1
Simone Piraccini <simopira01@gmail.com>
2022-11-24 17:26:12 +0100

    altri progressi

--- Prova_1/Caratteristica.cpp
+++ Prova_1/Caratteristica.cpp
@@ -29,7 +29,8 @@ void Caratteristica() {
   // Definisco i grafici
   TGraphErrors* silicio = new TGraphErrors("Dati_Silicio.txt", "%lg %lg %lg %lg");
   TGraphErrors* germanio = new TGraphErrors("Dati_Germanio.txt", "%lg %lg %lg %lg");
-  TGraphErrors* calibrazione = new TGraphErrors("Calibrazione_corretta.txt", "%lg %lg %lg %lg");
+  TGraphErrors* calibrazione = new TGraphErrors("Calibrazione_corretta.txt", "%lg %lg %lg %lg"); 
+  // bisogna modificare con gli errori in qudratura per l'oscilloscopio
   TGraphErrors* silicioLog = new TGraphErrors("Dati_Silicio.txt", "%lg %lg %lg %lg");
   TGraphErrors* germanioLog = new TGraphErrors("Dati_Germanio.txt", "%lg %lg %lg %lg");
 
