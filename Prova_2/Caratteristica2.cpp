c1a57cc1fb3e02840a93e5dc8de84f0bbceabc64
Simone Piraccini <simopira01@gmail.com>
2022-11-29 13:31:22 +0100

    progressi again

--- Prova_2/Caratteristica2.cpp
+++ Prova_2/Caratteristica2.cpp
@@ -1,16 +1,14 @@
 #include <cmath>
 #include <vector>
 
-void setStyle()
-{
+void setStyle() {
   gROOT->SetStyle("Plain");
   gStyle->SetPalette(57);
   gStyle->SetOptTitle(0);
 }
 
 // Funzione caratteristica I-V del diodo
-Double_t FitFunction(Double_t *x, Double_t *par)
-{
+Double_t FitFunction(Double_t *x, Double_t *par) {
   Double_t V_d = x[0];
   Double_t I_0 = par[0];
   Double_t V_t = par[1];
@@ -18,19 +16,31 @@ Double_t FitFunction(Double_t *x, Double_t *par)
   return val;
 }
 
-void Caratteristica2()
-{
+Double_t retta(Double_t *x, Double_t *par) {
+  Double_t ics = x[0];
+  Double_t emme = par[0];
+  Double_t qu = par[1];
+  Double_t val = emme * ics + qu;
+  return val;
+}
+
+void Caratteristica2() {
   // Definisco i grafici
   TGraphErrors *corrente_bassa = new TGraphErrors("Dati_ib_100.txt", "%lg %lg %lg %lg");
   TGraphErrors *corrente_alta = new TGraphErrors("Dati_ib_200.txt", "%lg %lg %lg %lg");
 
   // Assegno le funzioni di fit
-  TF1 *f1 = new TF1("silicio", FitFunction, 350, 760, 2);
-  TF1 *f2 = new TF1("germanio", FitFunction, 72, 320, 2);
+  TF1 *f1 = new TF1("ib_100", retta, -4500, -1000, 2);
+  TF1 *f2 = new TF1("ib_200", retta, -4500, -1000, 2);
 
   TCanvas *myCanvas1 = new TCanvas("Caratteristica con corrente 100 uA", "canvas 1");
   TCanvas *myCanvas2 = new TCanvas("Caratteristica con corrente 200 uA", "canvas 2");
 
+  corrente_bassa->Fit(f1, "R, C");
+  corrente_alta->Fit(f2, "R, C");
+
+  // Disegno i grafici
+  gStyle->SetOptFit(111);
   myCanvas1->SetGrid();
   myCanvas1->cd();
   corrente_bassa->Draw("APE, SAME");
