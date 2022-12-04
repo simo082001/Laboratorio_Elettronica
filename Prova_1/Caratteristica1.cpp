8de4eb984ef0cfd0ebb9b9d99b79b49601f680a0
Simone Piraccini <simopira01@gmail.com>
2022-12-04 16:46:38 +0100

    fit prova 1

--- Prova_1/Caratteristica1.cpp
+++ Prova_1/Caratteristica1.cpp
@@ -8,10 +8,10 @@ void setStyle() {
 
 // Funzione caratteristica I-V del diodo
 Double_t FitFunction(Double_t* x, Double_t* par) {
-  Double_t V_d = x[0];
-  Double_t I_0 = par[0];
-  Double_t V_t = par[1];
-  Double_t val = I_0 * (exp(V_d / V_t) - 1);
+  Double_t ics = log(x[0]);
+  Double_t emme = par[0];
+  Double_t qu = par[1];
+  Double_t val = emme * ics + qu;
   return val;
 }
 
@@ -34,13 +34,14 @@ void Caratteristica1() {
   TGraphErrors* germanioLog = new TGraphErrors("Dati_Germanio.txt", "%lg %lg %lg %lg");
 
   // Assegno le funzioni di fit
-  TF1* f1 = new TF1("silicio", FitFunction, 350, 760, 2);
-  TF1* f2 = new TF1("germanio", FitFunction, 72, 320, 2);
+  TF1* f1 = new TF1("silicio", FitFunction, 0, 8, 2);
+  TF1* f2 = new TF1("germanio", FitFunction, 0, 6, 2);
   TF1* f3 = new TF1("retta", retta, 0, 800, 2);
 
+
   // Devo settare i parametri iniziali
-  f1->SetParameters(5, 100);
-  f2->SetParameters(5, 100);
+  f1->SetParameters(50, 650);
+  f2->SetParameters(50, 250);
 
   // Cosmetica
   silicio->SetTitle("Curva caratteristica silicio");
@@ -85,18 +86,35 @@ void Caratteristica1() {
 
   silicio->GetYaxis()->SetTitleOffset(1);
   silicio->GetXaxis()->SetTitleOffset(1.2);
-  silicio->GetXaxis()->SetTitle("Tensione (mV)");
-  silicio->GetYaxis()->SetTitle("Intensità (mA)");
+  silicio->GetYaxis()->SetTitle("Tensione (mV)");
+  silicio->GetXaxis()->SetTitle("Intensita (mA)");
 
   germanio->GetYaxis()->SetTitleOffset(1);
   germanio->GetXaxis()->SetTitleOffset(1.2);
-  germanio->GetXaxis()->SetTitle("Tensione (mV)");
-  germanio->GetYaxis()->SetTitle("Intensità (mA)");
+  germanio->GetYaxis()->SetTitle("Tensione (mV)");
+  germanio->GetXaxis()->SetTitle("Intensita (mA)");
+
+  silicioLog->GetYaxis()->SetTitleOffset(1);
+  silicioLog->GetXaxis()->SetTitleOffset(1.2);
+  silicioLog->GetYaxis()->SetTitle("Tensione (mV)");
+  silicioLog->GetXaxis()->SetTitle("Intensita (mA)");
+
+  germanioLog->GetYaxis()->SetTitleOffset(1);
+  germanioLog->GetXaxis()->SetTitleOffset(1.2);
+  germanioLog->GetYaxis()->SetTitle("Tensione (mV)");
+  germanioLog->GetXaxis()->SetTitle("Intensita (mA)");
+
+  calibrazione->GetYaxis()->SetTitleOffset(1);
+  calibrazione->GetXaxis()->SetTitleOffset(1.2);
+  calibrazione->GetXaxis()->SetTitle("Tensione (mV)");
+  calibrazione->GetYaxis()->SetTitle("Tensione (mV)");
 
   // Fit dei grafici
   silicio->Fit(f1, "R, C");
   germanio->Fit(f2, "R, C");
-  calibrazione->Fit(f3, "R, C");
+  calibrazione->Fit(f3, "R");
+  silicioLog->Fit(f1, "R");
+  germanioLog->Fit(f2, "R");
 
   // Volendo c'è da creare le varie Canva
   TCanvas* myCanvas1 = new TCanvas("Caratteristica silicio", "canvas 1");
@@ -115,12 +133,12 @@ void Caratteristica1() {
   myCanvas1->cd();
   silicio->Draw("APE, SAME");
   myCanvas2->cd();
-  gPad->SetLogy();
+  gPad->SetLogx();
   silicioLog->Draw("APE, SAME");
   myCanvas3->cd();
   germanio->Draw("APE, SAME");
   myCanvas4->cd();
-  gPad->SetLogy();
+  gPad->SetLogx();
   germanioLog->Draw("APE, SAME");
   myCanvas5->cd();
   calibrazione->Draw("APE, SAME");
